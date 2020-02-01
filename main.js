b97f249901d626366870d77f76198b408e51d02a
Horacio Borrego <hborrego365@gmail.com>
2020-02-01 12:42:53 -0700

    Add left border to nav icons and hide function to wallet on click

--- main.js
+++ main.js
@@ -1,12 +1,50 @@
 
 var box = document.getElementById('oscarClose');
-  console.log(box);
-
+  // console.log(box);
 var closeIcon = document.getElementById('x_button');
-  console.log(closeIcon);
+  // console.log(closeIcon);
 var listen = closeIcon.addEventListener('click', removeBanner);
 
 function removeBanner() {
-  console.log('working');
+  // console.log('working');
   box.style.display = 'none';
 }
+
+var wallet = document.querySelector('.icons2');
+  // console.log(wallet);
+
+var dashboard = document.querySelector('.icons1');
+    // console.log(dashboard);
+
+var walletClick = document.addEventListener('click', hide);
+
+var mainAccts = document.querySelector('.mainAccts')
+
+var header = document.querySelector('.header')
+function hide() {
+  dashboard.style.borderLeft = 'none';
+  wallet.style.borderLeft = 'solid';
+  wallet.style.borderColor = '#1CA3BA';
+  aside.style.display = 'none';
+  box.style.display = 'none';
+  mainAccts.style.display = 'none';
+  header.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+// var walletClick2 = document.addEventListener('click', hideAside);
+
+var aside = document.querySelector('aside');
+
+
+
+
+// function hideAside(){
+//   aside.style.display = 'none';
+// }
